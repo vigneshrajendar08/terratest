5f0fbc5b115ee728affdfec3c8e1c4657cbffeaa
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-16 22:29:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+}
+
+variable "username" {
+  description = "Username for the database"
+  type        = string
+}
