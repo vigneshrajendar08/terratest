4195bccf1b7e9810371a17df38a103064973c375
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-16 16:28:04 +0530

    Update chukkus3.tf

--- chukkus3.tf
+++ chukkus3.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "chukku" {
   bucket = "chukkubucket"
 }
