497268921834c093645d184066549268ab8b49ec
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-16 23:30:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,3 @@ resource "aws_db_instance" "rds_instance" {
   db_name           = var.db_name
   username          = var.username
 }
-
-module "my_rds" {
-  source    = "./modules/aws-rds"
-  db_name   = "database"
-  username  = "admin"
-  # Provide values for other input variables
-}
