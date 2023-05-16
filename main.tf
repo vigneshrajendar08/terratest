f1305f32d60a1a9e0754bd128e6c05b7c928a64f
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-17 00:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-locals {
+#locals {
   
- password                = local.is_read_replica ? "" : var.snapshot_identifier == null ? local.pitr_restore == false ? random_id.password.hex : "" : ""
- skip_final_snapshot     = local.is_read_replica ? true : var.skip_final_snapshot
+ #password                = local.is_read_replica ? "" : var.snapshot_identifier == null ? local.pitr_restore == false ? random_id.password.hex : "" : ""
+ #skip_final_snapshot     = local.is_read_replica ? true : var.skip_final_snapshot
 
-}  
+#}  
 
 resource "aws_db_instance" "rds_instance" {  
   #identifier            = random_id.db_identifier.hex
@@ -11,12 +11,12 @@ resource "aws_db_instance" "rds_instance" {
   engine_version        = var.engine_version
   instance_class        = var.instance_class
   username              = var.username
-  password              = local.password
+  password              = var.password
   port                  = var.port
   parameter_group_name  = var.parameter_group_name
   storage_type          = var.storage_type
   allocated_storage     = var.allocated_storage
   max_allocated_storage = var.max_allocated_storage
-  skip_final_snapshot   = local.skip_final_snapshot
+  skip_final_snapshot   = var.skip_final_snapshot
   publicly_accessible   = false
 }
