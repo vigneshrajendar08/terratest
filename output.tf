6240f56ac37975294d02efcc6bd9b953a12fd647
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-17 00:22:27 +0530

    Update output.tf
    removed few lines in output.tf

--- output.tf
+++ output.tf
@@ -3,12 +3,3 @@ output "rds_endpoint" {
   value       = aws_db_instance.rds_instance.endpoint
   description = "The endpoint of the RDS instance"
 }
-output "id" {
-  description = "The RDS instance ID"
-  value       = aws_db_instance.id
-}
-output "arn" {
-  description = "The ARN of the RDS instance"
-  value       = aws_db_instance.arn
-}
-
