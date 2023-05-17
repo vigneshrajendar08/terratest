58941ff4fa2432450f0e96cb126a30d47181bf74
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-17 15:27:59 +0530

    lambda main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_db_instance" "chukku" { 
-  identifier = "gokulchukkuu1-rds-instance" 
-  allocated_storage = 10
-  storage_type = "gp2" 
-  engine = "postgres" 
-  engine_version = "13.4" 
-  instance_class = "db.t3.micro" 
-  name = "gokulchukkuu" 
-  username = "postgres" 
-  password = "password" 
-  parameter_group_name = "default.postgres13" 
-  publicly_accessible = false 
-  skip_final_snapshot = true 
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "my-lambda1"
+  description   = "My lambda function"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+
+  source_path = "../src/lambda-function1"
+
+  tags = {
+    Name = "my-lambda1"
+  }
 }
