35446d735fd6c02e7be3eab56d871f80d057d371
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-17 12:22:13 +0530

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 # Create output variables to expose useful information
-output "rds_endpoint" {
-  value       = aws_db_instance.rds_instance.endpoint
-  description = "The endpoint of the RDS instance"
+output "endpoint" {
+  description = "The connection endpoint"
+  value       = aws_db_instance.this.endpoint
 }
