f2fca6b549f7597757f3dd7f87bd39efb325e703
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-17 21:57:56 +0530

    lambda main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,43 @@
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "my-lambda-function"
+  handler      = "index.handler"
+  runtime      = "nodejs14.x"
+  memory_size  = 128
+  timeout      = 10
+  role         = aws_iam_role.lambda_role.arn
 
-  function_name = "my-lambda1"
-  description   = "My lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-
-  source_path = "../src/lambda-function1"
-
-  tags = {
-    Name = "my-lambda1"
-  }
+  // Specify the code location for your Lambda function
+  // You can use a local file or an S3 bucket
+  filename         = "lambda_function.zip"  # Local file
+  # or
+  s3_bucket        = "my-lambda-bucket"  # S3 bucket name
+  s3_key           = "lambda_function.zip"  # S3 object key
+  # or
+  # specify "image_uri" if using a container image
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+  
