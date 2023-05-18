a0d9edf3ce8975998e90c4c32068e84059902e40
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-18 13:45:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "archive" {}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "chukku_lambda.py"
+  output_path = "chukku_lambda.zip"
+}
+
 data "aws_iam_policy_document" "policy" {
   statement {
     sid    = ""
@@ -18,8 +26,18 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = "hello_lambda"
+  function_name = "chukku_lambda"
+
+  filename         = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
   role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello_lambda.lambda_handler"
+  handler = "chukku_lambda.lambda_handler"
   runtime = "python3.6"
-}  
+
+  environment {
+    variables = {
+      greeting = "HelloChukku"
+    }
+  }
+}
