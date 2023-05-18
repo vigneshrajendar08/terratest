77bc2d6590c4a693860046c9d29f4441a84f1df3
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-18 13:39:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda" {
   function_name = "hello_lambda"
-  role    = "${aws_iam_role.iam_for_lambda.arn}"
+  #role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.6"
 }  
