0be7cc4f8c695300b7d54cc6dbb86ff78cd8a4da
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-18 13:41:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name = "hello_lambda"
-  #role    = "${aws_iam_role.iam_for_lambda.arn}"
+  role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.6"
 }  
