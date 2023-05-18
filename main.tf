8e996e9ada8145cb528b27575e1a7451e5504f78
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-18 18:06:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,8 @@
-provider "archive" {}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-data "archive_file" "zip" {
-  type        = "zip"
-  source_file = "chukku_lambda.py"
-  output_path = "chukku_lambda.zip"
-}
-
-data "aws_iam_policy_document" "policy" {
-  statement {
-    sid    = ""
-    effect = "Allow"
-
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
-}
-
-resource "aws_lambda_function" "lambda" {
-  function_name = "chukku_lambda"
-
-  filename         = "${data.archive_file.zip.output_path}"
-  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
-
-  role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "chukku_lambda.lambda_handler"
-  runtime = "python3.9"
-
-  environment {
-    variables = {
-      greeting = "HelloChukku"
-    }
+  tags = {
+    Name = "main"
   }
 }
