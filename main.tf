e1c04df5f1c1030a86e55c4517d320921f7a2c86
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-19 15:15:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create the Lambda function
-resource "aws_lambda_function" "my_lambda" {
+resource "aws_lambda_function" "chukkulambda" {
   filename         = "lambda.zip"  # Replace with the path to your Lambda deployment package
-  function_name    = "my-lambda-function"
+  function_name    = "chukkulambda-function"
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.handler"
   runtime          = "nodejs14.x"  # Replace with your desired runtime
@@ -47,33 +47,33 @@ resource "aws_api_gateway_rest_api" "my_api" {
 }
 
 # Create the API Gateway resource
-resource "aws_api_gateway_resource" "my_api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+resource "aws_api_gateway_resource" "chukku_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.chukku_api.id
+  parent_id   = aws_api_gateway_rest_api.chukku_api.root_resource_id
   path_part   = "my-resource"
 }
 
 # Create the API Gateway method
-resource "aws_api_gateway_method" "my_api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.my_api_resource.id
+resource "aws_api_gateway_method" "chukku_api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.chukku_api.id
+  resource_id   = aws_api_gateway_resource.chukku_api_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 # Create the Lambda integration for API Gateway
-resource "aws_api_gateway_integration" "my_api_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.my_api_resource.id
-  http_method             = aws_api_gateway_method.my_api_method.http_method
+resource "aws_api_gateway_integration" "chukku_api_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.chukku_api.id
+  resource_id             = aws_api_gateway_resource.chukku_api_resource.id
+  http_method             = aws_api_gateway_method.chukku_api_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.my_lambda.invoke_arn
 }
 
 # Create the API Gateway deployment
-resource "aws_api_gateway_deployment" "my_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
+resource "aws_api_gateway_deployment" "chukku_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.chukku_api.id
   stage_name  = "test"
 }
 
