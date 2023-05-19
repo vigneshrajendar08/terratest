cadb3ad18540d56abb8821f291a67bf3dc99fa27
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-19 14:56:08 +0530

    lambda with api gateway main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,86 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# main.tf
 
-  tags = {
-    Name = "main"
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
+
+# Create the Lambda function
+resource "aws_lambda_function" "my_lambda" {
+  filename         = "lambda.zip"  # Replace with the path to your Lambda deployment package
+  function_name    = "my-lambda-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"  # Replace with your desired runtime
+
+  # Specify the environment variables for your Lambda function (if any)
+  environment {
+    variables = {
+      EXAMPLE_VARIABLE = "example_value"
+    }
   }
 }
+
+# Create the IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Attach the required policies to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Create the API Gateway REST API
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "my-api"
+  description = "My API"
+}
+
+# Create the API Gateway resource
+resource "aws_api_gateway_resource" "my_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "my-resource"
+}
+
+# Create the API Gateway method
+resource "aws_api_gateway_method" "my_api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_api_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# Create the Lambda integration for API Gateway
+resource "aws_api_gateway_integration" "my_api_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.my_api_resource.id
+  http_method             = aws_api_gateway_method.my_api_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.my_lambda.invoke_arn
+}
+
+# Create the API Gateway deployment
+resource "aws_api_gateway_deployment" "my_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "prod"
+}
+
