c227262b887e84218471ccbcb1f8f926d158ff02
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-22 00:18:40 +0530

    lambda changes in main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
-resource "aws_lambda_function" "Nissan_Aop_Lambda_Function" {
-  function_name = "Nissan-Aop-Lambda-Function"
-  runtime       = "python3.9"
-  handler       = "lambda_handler"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "Nissan_Aop_Lambda_Function.zip"
-  #source_code_hash = filebase64sha256("lambda_function.zip")
-}
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+  runtime = "nodejs8.10"
+}  
+
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
