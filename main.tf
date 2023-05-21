18bc699a148aee9822db31448d0d31498314d240
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-22 00:46:00 +0530

    last try main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
-  handler       = "exports.test"
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
-  runtime = "nodejs8.10"
-}  
-
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "Nissan-Aop-Lambda-Function"
+  runtime       = "python3.8"
+  handler       = "lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+}
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
