d4507a3f55049779b4ada049fb81aa1b63614e2c
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-21 14:27:23 +0530

    corrected naming conventions

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_api_gateway_method" "my_method" {
 resource "aws_lambda_permission" "apigw_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
+  function_name = aws_lambda_function.my_lambda_function.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }
@@ -57,5 +57,5 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method             = aws_api_gateway_method.my_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda.invoke_arn
+  uri                     = aws_lambda_function.my_lambda_function.invoke_arn
 }
