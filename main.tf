a2c93db42624927216a006e39cec011834bdbbd5
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-21 23:39:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,30 @@
-resource "aws_cognito_user_pool" "my_user_pool" {
-  name = "Nissan-Aop-user-pool"  # Replace with your desired user pool name
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
 
-  # Add additional configuration options as needed
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "Nissan-Aop-Lambda-Function"
+  runtime       = "Node.js 18.x "
+  handler       = "lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = "lambda_function.zip"
+  #source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
