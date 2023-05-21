d6ca5aa42e462d1d18afe170d2cb47b444d25ef2
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-21 15:14:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_lambda_function" "test_lambda" {
-  function_name = "my_lambda_function"
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my-lambda-function"
   runtime       = "python3.8"
   handler       = "lambda_handler"
   role          = aws_iam_role.lambda_role.arn
-  filename      = "my_lambda_function.zip"
-  source_code_hash = filebase64sha256("my_lambda_function.zip")
+  filename      = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -46,7 +46,7 @@ resource "aws_api_gateway_method" "my_method" {
 resource "aws_lambda_permission" "apigw_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.test_lambda.function_name
+  function_name = aws_lambda_function.my_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }
@@ -57,5 +57,5 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method             = aws_api_gateway_method.my_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.test_lambda.invoke_arn
+  uri                     = aws_lambda_function.my_lambda.invoke_arn
 }
