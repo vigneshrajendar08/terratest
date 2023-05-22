1a2ad43cf649ace4a972a1357a1674d1ce723251
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-22 17:56:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,6 @@ resource "aws_default_subnet" "default_az1" {
 resource "aws_default_subnet" "default_az2" {
    availability_zone = "us-east-1b"
 }
-  tags = {
-    Name = "Default subnet for us-east-1a us-east-1b"
-  }
-}
 
 data "aws_security_group" "default" {
   default = true
