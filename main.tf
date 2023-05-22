7ac32a12076764c85a8e71e30b79369500d5a78f
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-22 17:52:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,11 @@ resource "aws_api_gateway_rest_api" "Nissan_api" {
 
 resource "aws_default_subnet" "default_az1" {
   availability_zone = "us-east-1a"
-  availability_zone = "us-east-1b"
+}
 
+resource "aws_default_subnet" "default_az1" {
+   availability_zone = "us-east-1b"
+}
   tags = {
     Name = "Default subnet for us-east-1a us-east-1b"
   }
