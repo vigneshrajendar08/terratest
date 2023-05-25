8891c6ae51f374840d7673e27ad220ea5275000c
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-25 14:52:40 +0530

    lambda module test main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,14 @@
-# Create the API Gateway REST API
-resource "aws_api_gateway_rest_api" "Nissan_api" {
-  name        = "Nissan_api"
-  description = "My API"
-}
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
 
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1a"
-}
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
 
-resource "aws_default_subnet" "default_az2" {
-   availability_zone = "us-east-1b"
-}
+  source_path = "../src/lambda-function1"
 
-data "aws_security_group" "default" {
-  filter {
-    name   = "group-name"
-    values = ["default"]
+  tags = {
+    Name = "my-lambda1"
   }
-}
-
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
-
-
-#data "aws_security_group" "default" {
-  #default = true
-#}
-
-#resource "aws_instance" "example" {
-  #ami           = "ami-12345678"  # Replace with your desired AMI
-  #instance_type = "t2.micro"
-  #vpc_security_group_ids = [aws_security_group.example.id]
-  #subnet_id = data.aws_vpc.default.default_subnet_id
-
-resource "aws_vpc_endpoint" "my_endpoint" {
-  vpc_id         = data.aws_vpc.selected.id         # Replace with the ID of your VPC
-  service_name   = "com.amazonaws.us-east-1.account"  # Replace with the desired service name
-  vpc_endpoint_type = "Gateway"           # Replace with the desired endpoint type
-  #subnet_id = data.aws_vpc.default.default_subnet_id
-
-  # Add additional configuration options as needed
 }
