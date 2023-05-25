34b71e707c6d978e0feda604dee61ece6c8a5bef
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-25 14:53:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,9 +11,3 @@ provider "aws" {
   region = "us-east-1"
   # Add your AWS credentials or use environment variables or other authentication methods
 }
-
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
-  # Add your AWS credentials or use environment variables or other authentication methods
-}
