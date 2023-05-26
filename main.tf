76b37979402f99d5aad5503cdfa608e544eb3466
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-26 16:49:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,46 +3,11 @@ data archive_file lambda {
   source_file = "index.js"
   output_path = "lambda_function.zip"
 }
-
-resource aws_iam_role iam {
-  name = "iam_for_lambda_tf"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-module lambda {
-  source = "github.com/terraform-module/terraform-aws-lambda?ref=v2.12.8"
-
-  function_name  = "nissan-aop-lambda"
-  filename       = data.archive_file.lambda.output_path
-  description    = "description should be here"
-  handler        = "index.handler"
-  runtime        = "nodejs18.x"
-  memory_size    = "128"
-  concurrency    = "10"
-  lambda_timeout = "20"
-  log_retention  = "1"
-  role_arn       = aws_iam_role.iam.arn
-
-  environment = {
-    Environment = "test"
-  }
-
-  tags = {
-    Environment = "test"
-  }
+module "lambda" {
+  source  = "cloudposse/lambda-function/aws"
+  #version = "xxxx"
+  filename      = "lambda_function.zip"
+  function_name = "Nissan-Aop-function"
+  handler       = "handler.handler"
+  runtime       = "nodejs14.x"
 }
