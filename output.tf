b054b9467937db3748e836453594568ff4d7e76a
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-26 17:43:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
 output "lambda_function_arn" {
-  value = module.lambda.lambda_function_arn
-}
-output "api_gateway_id" {
-  value = module.api-gateway.api_gateway_id
+  value = module.lambda.function_arn
 }
