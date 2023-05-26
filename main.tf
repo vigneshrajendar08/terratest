e33432aa3c99019543d0d9213433423270277957
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-26 12:05:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,38 @@
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "hello_lambda.py"
+  output_path = "hello_lambda.zip"
+}
 
-  function_name = "my-lambda1"
-  description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+resource "aws_iam_role" "lambda_role" {
+  name = "nissan-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-  source_path = "../src/lambda-function1"
+module lambda {
+  source = "github.com/terraform-module/terraform-aws-lambda?ref=v2.12.8"
 
-  tags = {
-    Name = "my-lambda1"
-  }
+  function_name  = "Nissan-Aop-Lambda-Function"
+  filename       = data.archive_file.lambda.output_path
+  description    = "lambda function"
+  handler        = "hello_lambda.lambda_handler"
+  runtime        = "python3.8"
+  #memory_size    = "128"
+  #concurrency    = "5"
+  #lambda_timeout = "20"
+  #log_retention  = "1"
+  role_arn       = aws_iam_role.lambda_role.arn
 }
