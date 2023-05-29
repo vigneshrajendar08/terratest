e857cb7222d649c71376d33b44104ad5ac4fac67
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-29 15:46:55 +0530

    nlb api gateway commit

--- main.tf
+++ main.tf
@@ -1,45 +1,18 @@
-  module "nlb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+module "nlb" {
+  source  = "babbel/nlb-for-apigateway-vpc-link/aws"
+  version = "~> 1.1"
 
-  name = "my-nlb"
+  name = "example"
 
-  load_balancer_type = "network"
+  vpc = aws_vpc.this
 
-  vpc_id  = "vpc-0f2ab3f641f1f73a0"
-  subnets = ["subnet-092f810cc59683e1e", "subnet-0a5747a7c6ca78afa"]
-
-  #access_logs = {
-    #bucket = "my-nlb-logs"
-  #}
-
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "TCP"
-      backend_port     = 80
-      target_type      = "ip"
-    }
-  ]
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "TLS"
-      #certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      target_group_index = 0
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "TCP"
-      target_group_index = 0
-    }
-  ]
+  cidr_blocks = {
+    eu-west-1a = cidrsubnet(aws_vpc.this, 8, 0)
+    eu-west-1b = cidrsubnet(aws_vpc.this, 8, 1)
+  }
 
   tags = {
-    Environment = "Test"
+    app = "example"
+    env = "test"
   }
 }
