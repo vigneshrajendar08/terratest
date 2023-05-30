f4a4b1bda939578555f0db4d0590bfcb73b9745a
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-30 17:18:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,36 @@
+data archive_file lambda {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda_function.zip"
+}
+module "lambda" {
+  source  = "cloudposse/lambda-function/aws"
+  #version = "xxxx"
+  filename      = "lambda_function.zip"
+  function_name = "lambda_function"
+  handler       = "handler.handler"
+  runtime       = "nodejs14.x"
+}
+  
+module "api-gateway" {
+    source        = "clouddrove/api-gateway/aws"
+    version       = "1.0.1"
+    name          = "chukku-Aop-api-gateway"
+    environment   = "test"
+    label_order   = ["name", "environment"]
+    enabled       = true
+    #vpc_endpoint_ids = ["private"]
+    vpc_link_names = ["Nissan-Aop-Vpc"]
+    #vpc_link_tags =  {
+      #value = "Nissan-Aop-Vpc"
+    #}  
+}
+  
 module "nlb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "chukku-nlb"
+  name = "Nissan-Aop-nlb"
 
   load_balancer_type = "network"
 
@@ -10,5 +38,5 @@ module "nlb" {
   subnets = ["subnet-092f810cc59683e1e", "subnet-0a5747a7c6ca78afa"]
 
   #access_logs = {
-    #bucket = "chukku-nlb-logs"
+    #bucket = "Nissan-Aop-nlb-logs"
   }
