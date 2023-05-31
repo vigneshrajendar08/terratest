ba89fd859121371838a0577d5980d3839927b915
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-31 21:42:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+module "label" {
+  source  = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version, though usually you want to use the current one
+  # version = "x.x.x"
+  namespace = "eg"
+  name      = "example"
+}
+  
 module "route53_hostname" {
   source  = "cloudposse/route53-cluster-hostname/aws"
   # Cloud Posse recommends pinning every module to a specific version
