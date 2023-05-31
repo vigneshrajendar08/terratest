61c580792a1c7211e89ea223dc2bc03847da4b46
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-05-31 22:33:12 +0530

    SNS main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,10 @@
-locals {
-  enabled = module.this.enabled
-}
+module "sns_topic" {
+  source  = "terraform-aws-modules/sns/aws"
 
-data "aws_route53_zone" "default" {
-  count = module.this.enabled ? 1 : 0
+  name  = "simple"
 
-  name         = var.zone_name
-  private_zone = var.private_zone
-  zone_id      = var.zone_id
-  vpc_id       = var.zone_vpc_id
-  tags         = var.zone_tags
-}
-
-module "label" {
-  source  = "cloudposse/label/null"
-  # Cloud Posse recommends pinning every module to a specific version, though usually you want to use the current one
-  # version = "x.x.x"
-  namespace = "eg"
-  name      = "example"
-}
-  
-module "route53_hostname" {
-  source  = "cloudposse/route53-cluster-hostname/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  #version = "0.13.0"
-
-  zone_id = "Z3SO0TKDDQ0RGG"
-  type    = "CNAME"
-
-  records = [
-    "test-hostname",
-  ]
-
-  context = module.label.context
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
