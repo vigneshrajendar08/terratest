07f4d0ce80cd8f23c7fa1d87b81dfcc66333050a
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-02 16:31:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,14 @@
-data archive_file lambda {
-  type        = "zip"
-  source_file = "index.js"
-  output_path = "lambda_function.zip"
-}
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  #filename      = "Nissan-Aop-lambda_function.zip"
-  function_name = "Nissan-Aop-lambda"
-  description   = "Nissan lambda function"
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
   handler       = "index.lambda_handler"
-  runtime       = "nodejs14.x"
+  runtime       = "python3.8"
 
- 
+  source_path = "../src/lambda-function1"
+
   tags = {
-    Name = "Nissan-Aop-lambda"
+    Name = "my-lambda1"
   }
 }
-  
-  
-
-#module "lambda" {
-  #source  = "cloudposse/lambda-function/aws"
-  #version = "0.5.1"
-  #filename      = "lambda_function.zip"
-  #function_name = "lambda_function"
-  #handler       = "handler.handler"
-  #runtime       = "nodejs14.x"
-#}
-  
