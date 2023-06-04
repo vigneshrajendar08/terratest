3df9bbfb041771400ffc55a9477cfd419dbaf2c8
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-04 13:23:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,39 @@
-data archive_file lambda {
-  type        = "zip"
-  source_file = "index.js"
-  output_path = "lambda_function.zip"
-}
-module "lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "0.5.1"
-  filename      = "lambda_function.zip"
-  function_name = "lambda_function"
-  handler       = "handler.handler"
-  runtime       = "nodejs14.x"
+module "my_example_module" {
+  source                           = "../" # in this example, this is a local module. For real use, source will be "OpenClassrooms/lambda-apigw-module/aws"
+  lambda_project_name              = "test"
+  lambda_script_name               = "main"
+  lambda_handler                   = "main"
+  lambda_runtime                   = "python3.8"
+  lambda_code_repo                 = "https://github.com/xxx/myrepo"
+  lambda_codebase_bucket           = aws_s3_bucket.my-lambda-codebase-bucket.bucket
+  lambda_cloudwatch_logs_retention = 7
+  environment_variables = {
+    "staging" = {
+      MY_GREAT_VARIABLE = "bob",
+    }
+    "production" = {
+      MY_GREAT_VARIABLE = "alice",
+    }
+  }
+  use_api_gateway                       = true
+  use_api_gateway_api_key               = true
+  store_api_keys_in_ssm                 = true
+  api_gateway_domain_name_mapping       = var.apigw_domain_name_mapping
+  api_gateway_path                      = "mypath"
+  api_gateway_stages                    = ["production", "staging"]
+  api_gateway_cloudwatch_logs_retention = 7
+  send_logs_to_newrelic                 = true
+  newrelic_log_ingestion_function_arn   = "xxx"
+  scheduling_enabled                    = true
+  scheduling_config = {
+    "every_ten_minutes" = {
+      scheduling_expression = "cron(*/10 * * * ? *)",
+      input                 = "{\"foo\":{\"bar\": [1,2]}}"
+    }
+    "every_ten_minutes_plus_one" = {
+      scheduling_expression = "cron(1/10 * * * ? *)",
+      input                 = "{\"foo2\":{\"bar2\": [3,4]}}"
+    }
+  }
+  tags = var.tags
 }
