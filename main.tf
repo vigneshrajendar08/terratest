02337a824dff3c3f9372c40decca88ebf78ef905
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-04 13:44:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "my_example_module" {
   source        = "terraform-aws-modules/lambda/aws"
-  filename      = "lambda_function.zip"
+  #filename      = "lambda_function.zip"
   function_name = "lambda_function"
   handler       = "handler.handler"
   runtime       = "nodejs14.x"
