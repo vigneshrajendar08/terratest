f7fc12ed20c9481a9ed2a4ea3578b7028203496b
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-05 16:16:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,20 +6,3 @@ module "lambda" {
   handler       = "handler.handler"
   runtime       = "nodejs14.x"
 }
-
-module "api_gateway" {
-  source = "hashicorp/api_gateway/aws"
-
-  name = "hello_world"
-  region = "us-east-1"
-
-  rest_api {
-    description = "A simple REST API"
-  }
-
-  method {
-    resource_id = "${module.lambda.function_arn}"
-    method_type = "GET"
-    uri = "/"
-  }
-}
