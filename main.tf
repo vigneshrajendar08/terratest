2e80927748c7726b3e75b66f44ab19fb2746572a
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-05 13:28:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
-module "my_example_module" {
-  source        = "terraform-aws-modules/lambda/aws"
-  #filename      = "lambda_function.zip"
-  function_name = "lambda_function"
-  handler       = "handler.handler"
-  runtime       = "nodejs14.x"
+module "api_gateway_lambda_integration" {
+  source  = "infrablocks/api-gateway-lambda-integration/aws"
+  version = "2.0.0"
+
+  region = "us-east-1"
+
+  component             = "lambda-resource"
+  deployment_identifier = "production"
+
+  api_gateway_rest_api_id               = "x3H41ka22w"
+  api_gateway_rest_api_root_resource_id = "321pyrfif2"
+
+  lambda_function_name = "lambda-function"
 }
