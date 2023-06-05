7d92499fc8f3a5fa1948749a513e932d2c167511
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-05 17:13:04 +0530

    test r53 main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,49 @@
-module "lambda" {
-  source = "hashicorp/lambda/aws"
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
 
-  filename      = "lambda_function.zip"
-  function_name = "lambda_function"
-  handler       = "handler.handler"
-  runtime       = "nodejs14.x"
+  zones = {
+    "terraform-aws-modules-example.com" = {
+      comment = "terraform-aws-modules-examples.com (production)"
+      tags = {
+        env = "testing"
+      }
+    }
+
+    "myapp.com" = {
+      comment = "myapp.com"
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "apigateway1"
+      type    = "A"
+      alias   = {
+        name    = "d-10qxlbvagl.execute-api.us-east-1.amazonaws.com"
+        zone_id = "ZLY8HYME6SFAD"
+      }
+    },
+    {
+      name    = ""
+      type    = "A"
+      ttl     = 3600
+      records = [
+        "10.10.10.10",
+      ]
+    },
+  ]
+
+  depends_on = [module.zones]
 }
