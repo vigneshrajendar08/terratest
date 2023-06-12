58e0b523457c761b9557489d25e94011de566355
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-12 15:40:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,11 @@
 module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  function_name = "lambda-with-layer"
-  description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-  publish       = true
-
-  source_path = "../src/lambda-function1"
-
-  store_on_s3 = true
-  s3_bucket   = "my-bucket-id-with-lambda-builds"
-
-  layers = [
-    module.lambda_layer_s3.lambda_layer_arn,
-  ]
-
-  environment_variables = {
-    Serverless = "Terraform"
-  }
-
-  tags = {
-    Module = "lambda-with-layer"
-  }
+  source        = "./path/to/lambda-module"
+  function_name = "my-lambda-function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  source_code   = "./path/to/source_code"
 }
 
-module "lambda_layer_s3" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  create_layer = true
-
-  layer_name          = "lambda-layer-s3"
-  description         = "My amazing lambda layer (deployed from S3)"
-  compatible_runtimes = ["python3.8"]
-
-  source_path = "../src/lambda-layer"
-
-  store_on_s3 = true
-  s3_bucket   = "my-bucket-id-with-lambda-builds"
+output "lambda_function_arn" {
+  value = module.lambda_function.lambda_function_arn
 }
