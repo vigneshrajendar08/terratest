4dc39f43916b2778fc193e97efe9bfd3ebe3ced2
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-12 20:01:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-module "lambda_function" {
-  source        = "./path/to/lambda-module"
-  function_name = "my-lambda-function"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  source_code   = "./path/to/source_code"
+module "Lambda" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  environment_name        	= var.EnvironmentName
+  lambda_runtime          	= var.LambdaRuntime
+  lambda_timeout          	= var.LambdaTimeOut
+  lambda_handler          	= var.LambdaHandler
+  lambda_memory_size      	= var.LambdaMemorySize
+  env_variables           	= var.EnvVariables
+  security_group          	= var.SecurityGroup
+  log_retention_days      	= var.LogRetentionDays
+  LambdaErrorThreshold	  	= var.LambdaErrorThreshold
+  SNSTopic					= var.SNSTopic
+  EfsMountPath				= var.EfsMountPath
+  EfsAccessPointArn			= var.EfsAccessPointArn
+  PackageType				= var.PackageType
+  ReservedConcurrency		= var.ReservedConcurrency
+  ProvisionedConcurrency	= var.ProvisionedConcurrency
+  InsightsExtensionVersion	= var.InsightsExtensionVersion
+  LambdaLayer1				= var.LambdaLayer1
+  LambdaLayer2				= var.LambdaLayer2
+  
 }
