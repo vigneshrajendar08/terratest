5869265cebd996fcc3c08c806cf697e911645335
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-13 15:32:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ module "Lambda" {
   LambdaErrorThreshold	  	= "0.25"
   SNSTopic					        = "chukku"
   #EfsMountPath			      	= "your_efs_mount_path"
-  #EfsAccessPointArn			    = "your_efs_access_point_arn"
-  PackageType				        = ".zip"
-  #ReservedConcurrency	    	= your_reserved_concurrency
+  #EfsAccessPointArn			  = "your_efs_access_point_arn"
+  PackageType				        = "chukku.zip"
+  #ReservedConcurrency	   	= your_reserved_concurrency
   #ProvisionedConcurrency  	= your_provisioned_concurrency
   #InsightsExtensionVersion	= "your_insights_extension_version"
-  LambdaLayer1				      = "chukku_lambda_layer_1"
+  LambdaLayer1				      = "chukku.zip"
   #LambdaLayer2				      = "your_lambda_layer_2"
 
 
