0b4c7744a3ebb0130b6a1eb066cb9ae9c9bdf20c
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-21 13:24:24 +0530

    default lambda module

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-data archive_file lambda {
-  type        = "zip"
-  source_file = "index.js"
-  output_path = "lambda_function.zip"
-}
-module "lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "5.0.0"
-  #filename      = "lambda_function.zip"
-  function_name = "lambda_function"
-  handler       = "handler.handler"
-  runtime       = "nodejs14.x"
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+
+  source_path = "../src/lambda-function1"
+
+  tags = {
+    Name = "my-lambda1"
+  }
 }
