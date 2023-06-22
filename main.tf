0a6e268198da4fb9a56521c67c826a6ab8698c90
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 00:55:00 +0530

    api gateway

--- main.tf
+++ main.tf
@@ -1,13 +1,43 @@
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
-  create = true # to disable all resources
+  name          = "dev-http"
+  description   = "My awesome HTTP API Gateway"
+  protocol_type = "HTTP"
 
-  create_api_gateway               = true  # to control creation of API Gateway
-  create_api_domain_name           = true  # to control creation of API Gateway Domain Name
-  create_default_stage             = true  # to control creation of "$default" stage
-  create_default_stage_api_mapping = true  # to control creation of "$default" stage and API mapping
-  create_routes_and_integrations   = true  # to control creation of routes and integrations
-  create_vpc_link                  = true  # to control creation of VPC link
+  cors_configuration = {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
 
+  # Custom domain
+  domain_name                 = "terraform-aws-modules.modules.tf"
+  domain_name_certificate_arn = "arn:aws:acm:eu-west-1:052235179155:certificate/2b3a7ed9-05e1-4f9e-952b-27744ba06da6"
+
+  # Access logs
+  default_stage_access_log_destination_arn = "arn:aws:logs:us-east-1:579484639223:log-group:debug-apigateway"
+  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+
+  # Routes and integrations
+  integrations = {
+    "POST /" = {
+      lambda_arn             = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+
+    "GET /some-route-with-authorizer" = {
+      integration_type = "HTTP_PROXY"
+      integration_uri  = "some url"
+      authorizer_key   = "azure"
+    }
+
+    "$default" = {
+      lambda_arn = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
+    }
+  }
+  tags = {
+    Name = "http-apigateway"
+  }
 }
