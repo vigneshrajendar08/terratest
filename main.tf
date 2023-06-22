0176cb8698a6bc372cfa242d6e01f75b7f65de06
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 00:38:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
-  create = false # to disable all resources
+  create = true # to disable all resources
 
   create_api_gateway               = true  # to control creation of API Gateway
-  create_api_domain_name           = false  # to control creation of API Gateway Domain Name
-  create_default_stage             = false  # to control creation of "$default" stage
-  create_default_stage_api_mapping = false  # to control creation of "$default" stage and API mapping
-  create_routes_and_integrations   = false  # to control creation of routes and integrations
+  create_api_domain_name           = true  # to control creation of API Gateway Domain Name
+  create_default_stage             = true  # to control creation of "$default" stage
+  create_default_stage_api_mapping = true  # to control creation of "$default" stage and API mapping
+  create_routes_and_integrations   = true  # to control creation of routes and integrations
   create_vpc_link                  = true  # to control creation of VPC link
 
 }
