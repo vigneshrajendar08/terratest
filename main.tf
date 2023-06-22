8880a06e6618bb73c4ca5fbdc834fcbfe39395c1
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 00:22:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
 
-module "api-gateway" {
-    source        = "clouddrove/api-gateway/aws"
-    version       = "1.0.1"
-    name          = "Nissan-Aop-api-gateway"
-    environment   = "test"
-    label_order   = ["name", "environment"]
-    enabled       = true
-    #vpc_endpoint_ids = ["private"]
-    vpc_link_names = ["Nissan-Aop-Vpc"]
-    #vpc_link_tags =  {
-      #value = "Nissan-Aop-Vpc"
-    #}  
+  create = false # to disable all resources
+
+  create_api_gateway               = true  # to control creation of API Gateway
+  create_api_domain_name           = false  # to control creation of API Gateway Domain Name
+  create_default_stage             = false  # to control creation of "$default" stage
+  create_default_stage_api_mapping = false  # to control creation of "$default" stage and API mapping
+  create_routes_and_integrations   = false  # to control creation of routes and integrations
+  create_vpc_link                  = true  # to control creation of VPC link
+
 }
-  
-module "nlb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name = "Nissan-Aop-nlb"
-
-  load_balancer_type = "network"
-
-  vpc_id  = "vpc-0f2ab3f641f1f73a0"
-  subnets = ["subnet-092f810cc59683e1e", "subnet-0a5747a7c6ca78afa"]
-
-  #access_logs = {
-    #bucket = "Nissan-Aop-nlb-logs"
-  }
-
