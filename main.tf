ca65cb35a6d86209e3110d963d0d15236212c2a2
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-22 13:29:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,32 +11,3 @@ module "lambda" {
   handler       = "handler.handler"
   runtime       = "nodejs14.x"
 }
-  
-module "api-gateway" {
-    source        = "clouddrove/api-gateway/aws"
-    version       = "1.0.1"
-    name          = "Nissan-Aop-api-gateway"
-    environment   = "test"
-    label_order   = ["name", "environment"]
-    enabled       = true
-    #vpc_endpoint_ids = ["private"]
-    vpc_link_names = ["Nissan-Aop-Vpc"]
-    #vpc_link_tags =  {
-      #value = "Nissan-Aop-Vpc"
-    #}  
-}
-  
-module "nlb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name = "Nissan-Aop-nlb"
-
-  load_balancer_type = "network"
-
-  vpc_id  = "vpc-0f2ab3f641f1f73a0"
-  subnets = ["subnet-092f810cc59683e1e", "subnet-0a5747a7c6ca78afa"]
-
-  #access_logs = {
-    #bucket = "Nissan-Aop-nlb-logs"
-  }
