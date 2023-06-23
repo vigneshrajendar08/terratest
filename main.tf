f1f962aef907eab7e753c3a42326b34f78e41596
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 14:35:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-module "api-gateway" {
-    source        = "clouddrove/api-gateway/aws"
-    version       = "1.0.1"
-    name          = "Nissan-Aop-api-gateway"
-    environment   = "test"
-    label_order   = ["name", "environment"]
-    enabled       = true
-    #vpc_endpoint_ids = ["private"]
-    vpc_link_names = ["Nissan-Aop-Vpc"]
-    #vpc_link_tags =  {
-      #value = "Nissan-Aop-Vpc"
-    #}  
+data archive_file lambda {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda_function.zip"
+}
+module "lambda" {
+  source  = "cloudposse/lambda-function/aws"
+  version = "0.5.1"
+  filename      = "lambda_function.zip"
+  function_name = "lambda_function"
+  handler       = "handler.handler"
+  runtime       = "nodejs14.x"
 }
