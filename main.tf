cd5f0f6c03c2f0d78d09249980777d6f7bff9478
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 12:21:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,54 @@
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
 
-module "nlb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  name          = "dev-http"
+  description   = "My awesome HTTP API Gateway"
+  protocol_type = "HTTP"
 
-  name = "chukku-nlb"
-
-  load_balancer_type = "network"
-
-  vpc_id  = "vpc-0f2ab3f641f1f73a0"
-  subnets = ["subnet-07fe1c20051158233", "subnet-0ce8b754739746929"]
-
-  access_logs = {
-    bucket = "chukku-nlb-logs"
+  cors_configuration = {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
   }
 
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "TCP"
-      backend_port     = 80
-      target_type      = "ip"
-    }
-  ]
+  # Custom domain
+  domain_name                 = "terraform-aws-modules.modules.tf"
+  domain_name_certificate_arn = "arn:aws:acm:us-east-1:579484639223:certificate/2b3a7ed9-05e1-4f9e-952b-27744ba06da6"
 
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "TLS"
-      certificate_arn    = "arn:aws:iam::579484639223:server-certificate/test_cert-123456789012"
-      target_group_index = 0
-    }
-  ]
+  # Access logs
+  default_stage_access_log_destination_arn = "arn:aws:logs:eu-west-1:835367859851:log-group:debug-apigateway"
+  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "TCP"
-      target_group_index = 0
+  # Routes and integrations
+  integrations = {
+    "POST /" = {
+      lambda_arn             = "arn:aws:lambda:us-east-1:579484639223:function:my-function"
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
     }
-  ]
+
+    "GET /some-route-with-authorizer" = {
+      integration_type = "HTTP_PROXY"
+      integration_uri  = "some url"
+      authorizer_key   = "aws"
+    }
+
+    "$default" = {
+      lambda_arn = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
+    }
+  }
+
+  authorizers = {
+    "aws" = {
+      authorizer_type  = "JWT"
+      identity_sources = "$request.header.Authorization"
+      name             = "aws-auth"
+      audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
+      issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
+    }
+  }
 
   tags = {
-    Environment = "Test"
+    Name = "http-apigateway"
   }
 }
