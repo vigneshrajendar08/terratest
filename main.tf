e078828c7148629d86d24f6579c65ac2b20c16c1
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 14:02:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,13 @@
-module "api_gateway" {
-  source = "terraform-aws-modules/apigateway-v2/aws"
-
-  name          = "dev-http"
-  description   = "My awesome HTTP API Gateway"
-  protocol_type = "HTTP"
-
-  cors_configuration = {
-    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-    allow_methods = ["*"]
-    allow_origins = ["*"]
-  }
-
-  # Custom domain
-  domain_name                 = "terraform-aws-modules.nissan.com"
-  domain_name_certificate_arn = "arn:aws:acm-pca:us-east-1:579484639223:certificate-authority/953bf15f-3dd7-472b-a927-e6759f784397"
-
-  # Access logs
-  default_stage_access_log_destination_arn = "arn:aws:logs:us-east-1:579484639223:log-group:/aws/lambda/lambdafunction:*"
-  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
-
-
-  # Routes and integrations
-  integrations = {
-    "POST /" = {
-      lambda_arn             = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
-      payload_format_version = "2.0"
-      timeout_milliseconds   = 12000
-    }
-
-    "GET /some-route-with-authorizer" = {
-      integration_type = "HTTP_PROXY"
-      integration_uri  = "some url"
-      authorizer_key   = "azure"
-    }
-
-    "$default" = {
-      lambda_arn = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
-    }
-  }
-
-  authorizers = {
-    "azure" = {
-      authorizer_type  = "JWT"
-      identity_sources = "$request.header.Authorization"
-      name             = "azure-auth"
-      audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
-      issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
-    }
-  }
-
-  tags = {
-    Name = "http-apigateway"
-  }
+module "api-gateway" {
+    source        = "clouddrove/api-gateway/aws"
+    version       = "1.0.1"
+    name          = "Nissan-Aop-api-gateway"
+    environment   = "test"
+    label_order   = ["name", "environment"]
+    enabled       = true
+    #vpc_endpoint_ids = ["private"]
+    vpc_link_names = ["Nissan-Aop-Vpc"]
+    #vpc_link_tags =  {
+      #value = "Nissan-Aop-Vpc"
+    #}  
 }
