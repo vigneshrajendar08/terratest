35f638c43eb63fc3a7e7287794e485229a035583
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-23 12:42:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,6 @@ module "api_gateway" {
     allow_origins = ["*"]
   }
 
-# Custom domain
-  domain_name                 = "terraform-aws-modules.modules.tf"
-  domain_name_certificate_arn = "arn:aws:acm:us-east-1:579484639223:certificate/2b3a7ed9-05e1-4f9e-952b-27744ba06da6"
-
-  # Access logs
-  default_stage_access_log_destination_arn = "arn:aws:logs:us-east-1:579484639223:log-group:debug-apigateway"
-  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
-
   # Routes and integrations
   integrations = {
     "POST /" = {
@@ -39,10 +31,10 @@ module "api_gateway" {
   }
 
   authorizers = {
-    "azure" = {
+    "aws" = {
       authorizer_type  = "JWT"
       identity_sources = "$request.header.Authorization"
-      name             = "azure-auth"
+      name             = "aws-auth"
       audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
       issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
     }
