301be2c45833eb5d0a15a4182f0c4c67de63a56c
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-26 19:36:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "domain_name" {
+  description = "The custom domain name for the API Gateway"
+  type        = string
+  default     = "nissanaopdev.com"
+}
+
+variable "domain_name_certificate_arn" {
+  description = "The ARN of the ACM certificate for the custom domain name"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:579484639223:certificate/ac534e5d-7d37-4b49-b244-18c3c4359043"
+}
