2d9fcdfffc05b1cead73d30f5cc5da6c0ff36fae
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-26 18:09:01 +0530

    apigatwaymodule

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
-  name          = "dev-http"
-  description   = "My awesome HTTP API Gateway"
+  name          = "chukkuapi-http"
+  description   = "chukku HTTP API Gateway"
   protocol_type = "HTTP"
 
   cors_configuration = {
@@ -18,15 +18,15 @@ module "api_gateway" {
   # Routes and integrations
   integrations = {
     "POST /" = {
-      lambda_arn             = "arn:aws:lambda:eu-west-1:052235179155:function:my-function"
+      lambda_arn             = "arn:aws:lambda:us-east-1:579484639223:function:testforssns"
       payload_format_version = "2.0"
       timeout_milliseconds   = 12000
     }
 
     "GET /some-route-with-authorizer" = {
       integration_type = "HTTP_PROXY"
-      integration_uri  = "some url"
-      authorizer_key   = "aws"
+      integration_uri  = "http://reqres.in/img/faces/7-image.jpg"
+      authorizer_key   = "azure"
     }
 
     "$default" = {
@@ -35,10 +35,10 @@ module "api_gateway" {
   }
 
   authorizers = {
-    "aws" = {
+    "azure" = {
       authorizer_type  = "JWT"
       identity_sources = "$request.header.Authorization"
-      name             = "aws-auth"
+      name             = "azure-auth"
       audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
       issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
     }
