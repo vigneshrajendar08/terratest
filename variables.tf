c17773837a5419a398cf76cd938286f4ef4d54ff
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-27 16:14:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   description = "The custom domain name for the API Gateway"
   type        = string
-  default     = "www.nissan-aop.chukkuda.xyz"
+  default     = "*.chukkuda.xyz"
 }
 
 variable "domain_name_certificate_arn" {
   description = "The ARN of the ACM certificate for the custom domain name"
   type        = string
-  default     = "arn:aws:acm:us-east-1:579484639223:certificate/43146da5-b020-4a42-9709-d4c8c083fff5"
+  default     = "arn:aws:acm:us-east-1:579484639223:certificate/ad804ca3-99af-46f9-b9d7-771b5f2b74cc""
 }
