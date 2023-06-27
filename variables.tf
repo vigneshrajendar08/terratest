b8f409836aff3d86f81905c23f4ac436ea9b8272
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-27 16:35:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "domain_name" {
   description = "The custom domain name for the API Gateway"
   type        = string
-  default     = "*.chukkuda.xyz"
+  default     = "chukkuda.xyz"
 }
 
 variable "domain_name_certificate_arn" {
