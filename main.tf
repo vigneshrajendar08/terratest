d072987d3294cad98ea9c142fa92a0bc9539f025
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-27 14:32:44 +0530

    changed naming convention

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "apigateway-v2" {
   source  = "terraform-aws-modules/apigateway-v2/aws"
   version = "2.2.2"
 
-  name        = "my-http-api-gateway"
-  description = "My HTTP API Gateway"
+  name        = "aop-dev-api-gateway"
+  description = "nissan-aop-api-gateway"
 
   domain_name = var.domain_name
   domain_name_certificate_arn = var.domain_name_certificate_arn
