6ec7872f8b9a6bb7f4f9ce0294e02a2c6a643e31
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-06-27 15:02:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "domain_name" {
 variable "domain_name_certificate_arn" {
   description = "The ARN of the ACM certificate for the custom domain name"
   type        = string
-  default     = "arn:aws:acm:us-east-1:579484639223:certificate/030b582f-cc4d-43a5-a881-bce1d44a1005"
+  default     = "arn:aws:acm:us-east-1:579484639223:certificate/43146da5-b020-4a42-9709-d4c8c083fff5"
 }
