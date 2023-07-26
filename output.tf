91a5d3332b05a046d7277e65518f6b9943d9299d
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-07-26 16:03:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,29 +1,24 @@
 output "api_gateway_id" {
   description = "The ID of the API Gateway"
-  value       = module.api_gateway.api_gateway_id
+  value       = module.api_gateway.id
 }
 
-output "api_gateway_endpoint" {
-  description = "The endpoint URL of the API Gateway"
-  value       = module.api_gateway.api_gateway_endpoint
+output "api_gateway_name" {
+  description = "The name of the API Gateway"
+  value       = module.api_gateway.name
 }
 
-output "api_gateway_execution_arn" {
-  description = "The execution ARN of the API Gateway"
-  value       = module.api_gateway.api_gateway_execution_arn
-}
-
-output "api_gateway_deployment_id" {
-  description = "The deployment ID of the API Gateway"
-  value       = module.api_gateway.api_gateway_deployment_id
+output "api_gateway_protocol_type" {
+  description = "The protocol type of the API Gateway"
+  value       = module.api_gateway.protocol_type
 }
 
 output "api_gateway_domain_name" {
   description = "The domain name of the API Gateway"
-  value       = module.api_gateway.api_gateway_domain_name
+  value       = module.api_gateway.domain_name
 }
 
-output "api_gateway_domain_name_certificate_arn" {
-  description = "The ARN of the certificate used for the custom domain name"
-  value       = module.api_gateway.api_gateway_domain_name_certificate_arn
+output "api_gateway_integrations" {
+  description = "The integrations of the API Gateway"
+  value       = module.api_gateway.integrations
 }
