6bda51806c66052bdedfe662865a68335c9cd3f1
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-07-26 16:35:55 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,24 +1,14 @@
 output "api_gateway_id" {
-  description = "The ID of the API Gateway"
+  description = "ID of the API Gateway"
+  value       = module.api_gateway.apigatewayv2_api_id
+}
+
+output "api_gateway_execution_arn" {
+  description = "ARN of the API Gateway Execution"
+  value       = module.api_gateway.apigatewayv2_api_execution_arn
+}
+
+output "api_gateway_api_endpoint" {
+  description = "API Gateway endpoint URL"
   value       = module.api_gateway.apigatewayv2_api_api_endpoint
-}
-
-output "api_gateway_name" {
-  description = "The name of the API Gateway"
-  value       = module.api_gateway.name
-}
-
-output "api_gateway_protocol_type" {
-  description = "The protocol type of the API Gateway"
-  value       = module.api_gateway.protocol_type
-}
-
-output "api_gateway_domain_name" {
-  description = "The domain name of the API Gateway"
-  value       = module.api_gateway.domain_name
-}
-
-output "api_gateway_integrations" {
-  description = "The integrations of the API Gateway"
-  value       = module.api_gateway.integrations
 }
