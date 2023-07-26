3fdad34fd6a84a42af19526ddd3e7341d6613bff
vigneshrajendar08 <132331724+vigneshrajendar08@users.noreply.github.com>
2023-07-26 14:38:26 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "api_gateway_url" {
   description = "URL of the API Gateway"
-  value       = module.api_gateway.api_gateway_execution_arn
+  value       = module.api_gateway.http_api_url
 }
 
 output "api_gateway_id" {
   description = "ID of the API Gateway"
-  value       = module.api_gateway.api_gateway_id
+  value       = module.api_gateway.http_api_id
 }
 
 output "api_gateway_execution_arn" {
   description = "ARN of the API Gateway Execution"
-  value       = module.api_gateway.api_gateway_execution_arn
+  value       = module.api_gateway.http_api_arn
 }
 
 output "api_gateway_api_endpoint" {
   description = "API Gateway endpoint URL"
-  value       = module.api_gateway.api_gateway_api_endpoint
+  value       = var.api_gateway_api_endpoint
 }
